bfc87ab5862153d42d8be651c7afcc87136391ee
Ali Gündüz Ceylan <34925324+Aligndzcyln@users.noreply.github.com>
2022-02-03 00:07:29 +0300

    Some funtionality added and overall structure styled

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,52 +1,94 @@
-import React, {useState} from 'react';
-import QuestionCard from './components/QuestionCard';
-import { fetchQuizQuestions } from './API';
-import { Difficulty } from './API';
+import React, { useState } from "react";
+import QuestionCard from "./components/QuestionCard";
+import { fetchQuizQuestions } from "./API";
+import { Difficulty, QuestionState } from "./API";
+import { GlobalStyle, Wrapper } from "./App.styles";
 
 const totalQuestions = 10;
 
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
 const App = () => {
-
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setuserAnswers] = useState([]);
+  const [userAnswers, setuserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
-  const [gameOver, setgameOver] = useState(true);
-
-  console.log(fetchQuizQuestions(totalQuestions, Difficulty.EASY))
-  
+  const [gameOver, setGameOver] = useState(true);
 
   const startTrivia = async () => {
-
-  }
-  const checkAnswer = (e : React.MouseEvent<HTMLButtonElement>) => {
-    
-  }
+    setLoading(true);
+    setGameOver(false);
+    const newQuestions = await fetchQuizQuestions(
+      totalQuestions,
+      Difficulty.EASY
+    );
+    setQuestions(newQuestions);
+    setScore(0);
+    setuserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if(!gameOver){
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer
+      if(correct) setScore(prev => prev + 1);
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      }
+      setuserAnswers(prev => [...prev, answerObject]);
+    }
+  };
   const nextQuestion = () => {
-    
-  }
+    const nextQuestion = number + 1;
+    if(nextQuestion === totalQuestions){
+      setGameOver(true);
+    }else {
+      setNumber(nextQuestion);
+    }
+  };
   return (
-    <div className="App">
+    <>
+    <GlobalStyle />
+    <Wrapper>
       <h1>Quiz App</h1>
-      <button className='start' onClick={startTrivia}>
-        Start
-      </button>
-      <p className='score'>Score:</p>
-      <p>Loading questions...</p>
-      {/* <QuestionCard
-      questionNumber={number + 1}
-      totalQuestions={totalQuestions}
-      question={questions[number].question}
-      answers={questions[number].answers}
-      userAnswer={userAnswers ? userAnswers[number] : undefined}
-      callback={checkAnswer}
-      /> */}
-      <button className='next' onClick={nextQuestion}>
-        Next Question
-      </button>
-    </div>
+      {gameOver || userAnswers.length === totalQuestions ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver && <p className="score">Score:{score}</p>}
+      {loading && <p>Loading questions...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNumber={number + 1}
+          totalQuestions={totalQuestions}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== totalQuestions - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
+    </Wrapper>
+    </>
   );
-}
+};
 
 export default App;
--- quiz-app/src/utils.ts
+++ quiz-app/src/utils.ts
@@ -1,7 +1,5 @@
 import React from 'react';
 
-function utils() {
-  return ;
+export const shuffleArray = (array: any[]) => {
+  return [...array].sort(() => Math.random() - 0.5);
 }
-
-export default utils;
